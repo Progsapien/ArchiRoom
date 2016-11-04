94adaf31ec255e4b55535da3f6b101bac3a857d4
= <=>
2016-11-04 17:03:36 +0300

    Background logic 70%

---
+++ js/background_image.js
@@ -0,0 +1,61 @@
+﻿
+// Извлекаем объект;
+
+var background_image = document.getElementById("background_image");
+var background_images = [];
+var image_object = new Image();
+var image_index = 0;
+
+function parseImages() {
+    var request_images = new XMLHttpRequest();
+
+    request_images.open("GET", "php/parseImages.php", false);
+
+    request_images.onload = function () {
+        background_images = this.responseText.split("<sep>");
+        background_images.length = background_images.length - 1;
+        nextImage();
+        nextImage();
+        nextImage();
+        previousImage();
+
+    }
+    request_images.send();
+}
+
+function onResize() {
+    background_image.style.width = document.documentElement.clientWidth + "px";
+    background_image.style.height = document.documentElement.clientHeight + "px";
+}
+
+function onLoad() {
+    onResize();
+    background_image.style.backgroundColor = "black";
+    parseImages();
+}
+
+
+function nextImage() {
+    image_object.src = background_images[image_index++];
+    if (image_index == background_images.length) {
+        image_index = 0;
+    }
+
+    background_image.style.backgroundImage = "url(" + image_object.src + ")";
+}
+
+function previousImage() {
+    image_object.src = background_images[image_index--];
+    if (image_index < 0) {
+        image_index = background_images.length - 1;
+    };
+
+    background_image.style.backgroundImage = "url(" + image_object.src + ")";
+}
+
+setInterval(nextImage, 1000);
+
+// Сцепляем события с функцией;
+
+window.onload = onLoad;
+window.onresize = onResize;
---
+++ php/parseImages.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+
+$list_files = scandir("../projects/");
+foreach ($list_files as $item) {
+    if(is_dir("../projects/".$item)) {
+        foreach (scandir("../projects/".$item) as $item_project) {
+            if(strpos($item_project, ".jpg") || strpos($item_project, ".png")) {
+                echo "projects/".$item."/".$item_project."<sep>";
+            }
+        }
+    }
+}
+
+?>
