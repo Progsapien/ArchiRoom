5cdff2c82293828beca7b8ca75612af14d432784
= <=>
2016-11-08 22:20:54 +0300

    Fixed bugs

--- js/background_image.js
+++ js/background_image.js
@@ -1,55 +1,87 @@
 ﻿
 // Извлекаем объект;
+nextBackgroundImage = null;
+previousBackgroundImage = null;
+onLoadBackground = null;
+onResizeBackground = null;
 
-var background_image = document.getElementById("background_image");
-var background_images = [];
-var image_object = new Image();
-var image_index = 0;
-
-function parseImages() {
-    var request_images = new XMLHttpRequest();
-
-    request_images.open("GET", "php/parseImages.php", false);
-
-    request_images.onload = function () {
-        background_images = this.responseText.split("<sep>");
-        background_images.length = background_images.length - 1;
-        nextImage();
-    }
-    request_images.send();
-}
-
-function onResize() {
-    background_image.style.width = document.documentElement.clientWidth + "px";
-    background_image.style.height = document.documentElement.clientHeight + "px";
-}
-
-function onLoad() {
-    onResize();
-    background_image.style.backgroundColor = "black";
-    parseImages();
-}
+(function () {
+    var background_image = document.getElementById("background_image");
+    var background_images = [];
+    var image_object = new Image();
+    var image_index = 0;
+    var background_image_timer_id = -1;
+    var background_image_opacity = 0;
 
 
-function nextImage() {
-    image_object.src = background_images[image_index++];
-    if (image_index == background_images.length) {
-        image_index = 0;
+    function parseImages() {
+        var request_images = new XMLHttpRequest();
+
+        request_images.open("GET", "php/parseImages.php", true);
+
+        request_images.onload = function () {
+            background_images = this.responseText.split("<sep>");
+            background_images.length = background_images.length - 1;
+            nextImage();
+        }
+        request_images.send();
     }
 
-    background_image.style.backgroundImage = "url(" + image_object.src + ")";
-}
+    function onResize() {
+        background_image.style.width = document.documentElement.clientWidth + "px";
+        background_image.style.height = document.documentElement.clientHeight + "px";
+    }
 
-function previousImage() {
-    image_object.src = background_images[image_index--];
-    if (image_index < 0) {
-        image_index = background_images.length - 1;
-    };
+    function onLoad() {
+        onResize();
+        parseImages();
+    }
 
-    background_image.style.backgroundImage = "url(" + image_object.src + ")";
-}
 
-// Сцепляем события с функцией;
+    function nextImage() {
+        if (++image_index == background_images.length) {
+            image_index = 0;
+        }
+        clearInterval(background_image_timer_id);
+        background_image_timer_id = setInterval(hideImage, 1);
+    }
 
-window.onload = onLoad;
-window.onresize = onResize;
+    function previousImage() {
+        if (--image_index < 0) {
+            image_index = background_images.length - 1;
+        };
+        clearInterval(background_image_timer_id);
+        background_image_timer_id = setInterval(hideImage, 1);
+    }
+
+    function hideImage() {
+        console.log("HIDING");
+        if (background_image_opacity >= 0) {
+            background_image_opacity -= 0.1;
+            background_image.style.opacity = background_image_opacity;
+        } else {
+            clearInterval(background_image_timer_id);
+            image_object.src = background_images[image_index];
+        }
+    }
+
+    function showImage() {
+        if (background_image_opacity <= 1) {
+            background_image_opacity += 0.02;
+            background_image.style.opacity = background_image_opacity;
+        } else {
+            clearInterval(background_image_timer_id);
+        }
+    }
+
+    image_object.onload = function () {
+        clearInterval(background_image_timer_id);
+        background_image.style.backgroundImage = "url(" + image_object.src + ")";
+        background_image_timer_id = setInterval(showImage, 1);
+    }
+
+    nextImageF = nextImage;
+    previousImageF = previousImage;
+    onLoadBackground = onLoad;
+    onResizeBackground = onResize;
+}());
--- js/background_switcher.js
+++ js/background_switcher.js
@@ -1,24 +1,55 @@
 ﻿
-var buttons = document.getElementsByClassName("background_switcher_buttons");
+
+(function () {
+    var buttons = document.getElementsByClassName("background_switcher_buttons");
+    var background_switcher = document.getElementById("background_switcher");
 
 
-// Для 0 и 2 действия из background_image.js
+    // Для 0 и 2 действия из background_image.js
 
-buttons[0].onclick = previousImage;
-buttons[2].onclick = nextImage;
+    buttons[0].onclick = previousImageF;
+    buttons[2].onclick = nextImageF;
 
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].onmouseover = onOverButton;
-    buttons[i].onmouseout = onOutButton;
-}
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].onmouseover = onOverButton;
+        buttons[i].onmouseout = onOutButton;
+    }
 
-// ..
+    window.onresize = onResizeWindow
+    window.onload = onLoadWindow;
 
-function onOverButton() {
-    this.style.background = "white";
-    this.style.cursor = "pointer";
-}
+    // ..
 
-function onOutButton() {
-    this.style.background = "rgba(255,255,255,0.8)";
-}
+    function onOverButton() {
+        this.style.background = "rgba(200,200,200,0.8)";
+        this.style.cursor = "pointer";
+    }
+
+    function onOutButton() {
+        this.style.background = "rgba(255,255,255,0.8)";
+    }
+
+    function onResizeWindow() {
+
+        if (document.documentElement.clientWidth < 900) {
+            background_switcher.style.width = document.documentElement.clientWidth + "px";
+            background_switcher.style.left = "0px";
+            background_switcher.style.bottom = "0px";
+            background_switcher.style.right = "";
+        } else {
+            background_switcher.style.left = "";
+            background_switcher.style.top = "";
+            background_switcher.style.width = "300px";
+            background_switcher.style.right = "30px";
+            background_switcher.style.bottom = "30px";
+        }
+
+        onResizeBackground();
+    }
+
+    function onLoadWindow() {
+        onResizeWindow();
+        onLoadBackground();
+    }
+
+}())
