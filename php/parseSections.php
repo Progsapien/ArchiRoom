e16db57da3a79186725d99defbe93344e37d9d23
Valentin <progsapien@gmail.com>
2016-11-12 18:11:33 +0300

    parsing sections

--- php/parseSections.php
+++ php/parseSections.php
@@ -1,5 +1,15 @@
 ﻿<?php
 
-echo '<p class="menu_item">ВСЕ ПРОЕКТЫ</p><p class="menu_item">ЖИЛЫЕ</p><p class="menu_item">ИНТЕРЬЕРЫ</p>';
+$list_files = scandir("../projects/");
+foreach ($list_files as $dir_item) {
+    if(is_dir("../projects/".$dir_item) && $dir_item != ".." && $dir_item != ".") {
+        echo $dir_item;
+        if(file_exists("../projects/".$dir_item."/descript.txt")) {
+            echo file_get_contents("../projects/".$dir_item."/descript.txt");
+        } else {
+        }
+    }
+}
+echo "END.";
 
 ?>
